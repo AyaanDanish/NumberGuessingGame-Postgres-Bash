9d44215a1d96eea22fe34298a436e1e32aa570b9
AyaanDanish <ayaandanish7@gmail.com>
2023-08-03 17:14:41 +0000

    feat: get username from db

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+echo -e "\nEnter your username:"
+read USERNAME
+
+USERNAME_GET_RESULT=$($PSQL "select user_id, games_played, best_game from users where username = '$USERNAME'";)
+echo $USER_NAME_GET_RESULT
