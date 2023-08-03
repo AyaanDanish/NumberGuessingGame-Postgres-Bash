162786a1ba617034c99d933cac08b528045a20ea
AyaanDanish <ayaandanish7@gmail.com>
2023-08-03 18:33:01 +0000

    fix: changed user data destructuring method

--- number_guess.sh
+++ number_guess.sh
@@ -10,13 +10,11 @@ read USERNAME
 USER_DETAILS=$($PSQL "select user_id, games_played, best_game from users where username = '$USERNAME'";)
 if [[ -z $USER_DETAILS ]]
 then
-  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+  echo -e "Welcome, $USERNAME! It looks like this is your first time here."
   ENTER_USER_RESULT=$($PSQL "insert into users(username) values('$USERNAME');")
 else
-  echo $USER_DETAILS | while IFS="|" read USER_ID GAMES_PLAYED BEST_GAME
-  do
-    echo -e "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
-  done
+  IFS='|' read -r USER_ID GAMES_PLAYED BEST_GAME <<< "$USER_DETAILS"
+  echo -e "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
 GUESS_COUNT=0
@@ -33,11 +31,22 @@ do
 
   if [[ $GUESS > $RANDOM_NUM ]]
   then
-    echo -e -n "\nIt's lower than that, guess again:"
+    echo -e -n "\nIt's lower than that, guess again: "
   elif [[ $GUESS < $RANDOM_NUM ]]
   then
-     echo -e -n "\nIt's higher than that, guess again:"
+     echo -e -n "\nIt's higher than that, guess again: "
   fi
+
+  (( GUESS_COUNT++ ))
 done
 
-echo "right!"
+echo -e "\nYou guessed it in $GUESS_COUNT tries. The secret number was $RANDOM_NUM. Nice job!"
+
+if [[ $GUESS_COUNT < $BEST_GAME ]]
+then
+  echo "You did better!"
+  BEST_GAME=$GUESS_COUNT
+fi
+
+UPDATE_GAMES_PLAYED=$($PSQL "update users set games_played = games_played + 1;")
+UPDATE_BEST_GUESS=$($PSQL "update users set best_game = $BEST_GAME;")
